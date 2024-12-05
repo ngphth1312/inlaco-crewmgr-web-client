6ffdba9ebe304bda64eee2b9730352d09abae93b
ngphth1312 <ngphth1312@gmail.com>
2024-12-05 23:45:55 +0700

    feat: adding way to navigate between the Authentication and Main app section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { CssBaseline } from "@mui/material";
 import { TopBar, SideBar } from "./components/global";
 import { Routes, Route } from "react-router";
 
-// import LoginPage from "./pages/loginPage";
+import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
 import CrewInfos from "./pages/crewInfos";
 import CrewMobilization from "./pages/crewMobilization";
@@ -14,32 +14,38 @@ import CrewRecruitment from "./pages/crewRecruitment";
 import CrewTraining from "./pages/crewTraining";
 
 function App() {
+
+  const tempAccessToken = "qwo123ui12y3iu12y3i"; //Admin has logged in
+
   return (
     <>
       {/* Reset CSS to default */}
       <CssBaseline />
-      <div className="app">
-        <SideBar />
-        <main className="content">
-          <TopBar />
+      {tempAccessToken !== "" ? (
+        <div className="app">
+          <SideBar />
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/homePage" element={<HomePage />} />
+              <Route path="/crewInfos" element={<CrewInfos />} />
+              <Route path="/crewMobilization" element={<CrewMobilization />} />
+              <Route path="/crewContract" element={<CrewContract />} />
+              <Route path="/supplyContract" element={<SupplyContract />} />
+              <Route path="/templateContract" element={<TemplateContract />} />
+              <Route path="/supplyRequest" element={<SupplyRequest />} />
+              <Route path="/crewRecruitment" element={<CrewRecruitment />} />
+              <Route path="/crewTraining" element={<CrewTraining />} />
+            </Routes>
+          </main>
+        </div>
+      ) : (
+        <div className="login">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/crewInfos" element={<CrewInfos />} />
-            <Route path="/crewMobilization" element={<CrewMobilization />} />
-            <Route path="/crewContract" element={<CrewContract />} />
-            <Route path="/supplyContract" element={<SupplyContract />} />
-            <Route path="/templateContract" element={<TemplateContract />} />
-            <Route path="/supplyRequest" element={<SupplyRequest />} />
-            <Route path="/crewRecruitment" element={<CrewRecruitment />} />
-            <Route path="/crewTraining" element={<CrewTraining />} />
+            <Route path="/" element={<LoginPage />} />
           </Routes>
-        </main>
-      </div>
-      {/* <div className="login">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div> */}
+        </div>
+      )}
     </>
   );
 }
